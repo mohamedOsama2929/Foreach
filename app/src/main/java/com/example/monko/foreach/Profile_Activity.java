d5ba5998b2616f40b48f9d59fddf9548b1f610f9
monk996 <monkosama1996@gmail.com>
2017-04-27 14:08:03 +0200

    profile

--- app/src/main/java/com/example/monko/foreach/Profile_Activity.java
+++ app/src/main/java/com/example/monko/foreach/Profile_Activity.java
@@ -317,7 +317,7 @@ public class Profile_Activity extends MainActivity {
 
                                     } else {
 
-                                        counter++;
+                                        counter+=1;
                                         //viewHolder.setLikesCount(counter);
 
                                         Database.child(post_key).child("likes").setValue(counter);
